aabe326f483077e53da1d92c3be97b123616dd87
fernando2349 <marfprada@soy.sena.edu.co>
2023-10-25 15:33:34 -0500

    feat(conf): add connection string and packages entity framework

--- RestBilling/RestBilling/Program.cs
+++ RestBilling/RestBilling/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using RestBilling.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
